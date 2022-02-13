21a518a12e0fcfdc38fbb6e8e5477d46f2b3ad46
Idzanaagi <anmorgunowa@gmail.com>
2022-02-13 20:14:36 +0300

    step 2: delete 'name' and 'help', add arguments, option 'format'

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,9 +4,11 @@ import { Command } from "commander";
 const program = new Command();
 
 program
-    .name('gendiff')
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1', '-V, --version', 'output the version number')
-    .help('-H, --help', 'output usage information')
+.description('Compares two configuration files and shows a difference.')
+.argument('<filepath1>')
+.argument('<filepath2>')
+.version('0.1.0')
+.option('-f, --format <type>', 'output format');
+
 
 program.parse();
