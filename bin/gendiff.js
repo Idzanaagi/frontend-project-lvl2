8dfe47cd54dbf01cd2e163f93143d69596eac9b6
Idzanaagi <anmorgunowa@gmail.com>
2022-02-20 14:41:56 +0300

    step 4.1: fix after linter check

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 
-import { Command } from "commander";
-import genDiff from '../src/gendiff.js'
+import { Command } from 'commander';
+import genDiff from '../src/gendiff.js';
+
 const program = new Command();
 
 program
-.description('Compares two configuration files and shows a difference.')
-.argument('<filepath1>')
-.argument('<filepath2>')
-.version('0.1.0')
-.option('-f, --format <type>', 'output format')
-.action((filepath1, filepath2) => {
+  .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .version('0.1.0')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2, program.opts().format));
   });
 
