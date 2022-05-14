9a822014276e37575e6fff43cb8cc7d8db78635b
Idzanaagi <anmorgunowa@gmail.com>
2022-05-14 20:34:48 +0300

    add fixtures and modified tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,15 +1,39 @@
 import { test, expect } from '@jest/globals';
-import getTree from '../src/index.js';
+import genDiff from '../src/compareObjects.js';
 import { readFile } from '../parsers.js';
 
-const expectedResult = readFile('result.txt');
-const currentJson = getTree('file1.json', 'file2.json');
-const currentYaml = getTree('file1.yaml', 'file2.yml');
+const flatFileResult = readFile('result.txt');
+const fileFlatJson = genDiff('file1.json', 'file2.json');
+const fileFlatYaml = genDiff('file1.yaml', 'file2.yml');
 
-test('compare JSON files', () => {
-  expect(currentJson).toEqual(expectedResult);
+test('flatFileComparison', () => {
+  expect(fileFlatJson).toEqual(flatFileResult);
+  expect(fileFlatYaml).toEqual(flatFileResult);
 });
 
-test('compare YAML files', () => {
-  expect(currentYaml).toEqual(expectedResult);
+const nestedFileResult = readFile('resultNested.txt');
+const nestedJson = genDiff('file1nested.json', 'file2nested.json');
+const nestedYaml = genDiff('file1nested.yaml', 'file2nested.yml');
+
+test('nestedFileComparison', () => {
+  expect(nestedJson).toEqual(nestedFileResult);
+  expect(nestedYaml).toEqual(nestedFileResult);
+});
+
+const formatterPlainResult = readFile('resultPlain.txt');
+const nestedJsonPlain = genDiff('file1nested.json', 'file2nested.json', 'plain');
+const nestedYamlPlain = genDiff('file1nested.yaml', 'file2nested.yml', 'plain');
+
+test('formatterPlain', () => {
+  expect(nestedJsonPlain).toEqual(formatterPlainResult);
+  expect(nestedYamlPlain).toEqual(formatterPlainResult);
+});
+
+const formatterJsonResult = readFile('resultJSON.txt');
+const nestedJsonJSON = genDiff('file1nested.json', 'file2nested.json', 'json');
+const nestedYamlJSON = genDiff('file1nested.yaml', 'file2nested.yml', 'json');
+
+test('formatterJson', () => {
+  expect(nestedJsonJSON).toEqual(formatterJsonResult);
+  expect(nestedYamlJSON).toEqual(formatterJsonResult);
 });
