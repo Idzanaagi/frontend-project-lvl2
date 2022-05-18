03cf44a50fd04820fb496418a39e38c81ef7cd63
Idzanaagi <anmorgunowa@gmail.com>
2022-05-18 17:36:06 +0300

    modified parsers

--- parsers.js
+++ parsers.js
@@ -1,13 +1,11 @@
-/* eslint-disable no-underscore-dangle */
 import * as fs from 'fs';
 import * as yaml from 'js-yaml';
 import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const currentDir = process.cwd();
+const pathToFile = path.join(currentDir, '__fixtures__');
 
-export const readFile = (filename) => fs.readFileSync(path.join('__fixtures__', filename), 'utf-8');
+export const readFile = (filename) => fs.readFileSync(path.join(pathToFile, filename), 'utf-8');
 
 export const parsingFile = (fileName) => {
   const fileExtension = path.extname(fileName);
