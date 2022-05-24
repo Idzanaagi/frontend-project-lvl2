3f8c47215db552745f451b79452e0117b043adc9
Idzanaagi <anmorgunowa@gmail.com>
2022-05-24 19:03:58 +0300

    replace !Object.hasOwn to !_.has

--- src/compareObjects.js
+++ src/compareObjects.js
@@ -11,9 +11,9 @@ function genDiff(filename1, filename2) {
     const sortedKeys = _.sortedUniq(_.sortBy(keys)).map((key) => {
       if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
         return { type: 'nested', key, childs: buidTree(obj1[key], obj2[key]) };
-      } if (!Object.hasOwn(obj1, key)) {
+      } if (!_.has(obj1, key)) {
         return { type: 'added', key, value: obj2[key] };
-      } if (!Object.hasOwn(obj2, key)) {
+      } if (!_.has(obj2, key)) {
         return { type: 'removed', key, value: obj1[key] };
       } if (obj1[key] === obj2[key]) {
         return { type: 'same', key, value: obj1[key] };
