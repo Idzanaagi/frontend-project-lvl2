72e4ca2b064ac5f4fb6c00d852aae36a3f75670e
Idzanaagi <anmorgunowa@gmail.com>
2022-05-24 18:27:11 +0300

    compare

--- src/compareObjects.js
+++ src/compareObjects.js
@@ -2,12 +2,21 @@ import _ from 'lodash';
 import { parsingFile } from '../parsers.js';
 import chooseFormatters from './formatters/index.js';
 
-const genDiff = (filename1, filename2) => {
+
+const getDiff = (filename1, filename2, formatName = 'stylish') => {
+  const parseFile1 = parsingFile(filename1);
+  const parseFile2 = parsingFile(filename2);
+  const getTree = genDiff(parseFile1, parseFile2);
+  const getNewTree = chooseFormatters(getTree, formatName);
+  return getNewTree;
+};
+
+export default getDiff;
+
+function genDiff(filename1, filename2) {
   const buidTree = (obj1, obj2) => {
     const keys = Object.keys({ ...obj1, ...obj2 });
-    const sortedKeys = _.sortedUniq(_.sortBy(keys))
-
-      .map((key) => {
+    const sortedKeys = _.sortedUniq(_.sortBy(keys)).map((key) => {
         if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
           return { type: 'nested', key, childs: buidTree(obj1[key], obj2[key]) };
         } if (!Object.hasOwn(obj1, key)) {
@@ -22,16 +31,7 @@ const genDiff = (filename1, filename2) => {
         };
       });
     return sortedKeys;
+    
   };
   return buidTree(filename1, filename2);
 };
-
-const getDiff = (filename1, filename2, formatName = 'stylish') => {
-  const parseFile1 = parsingFile(filename1);
-  const parseFile2 = parsingFile(filename2);
-  const getTree = genDiff(parseFile1, parseFile2);
-  const getNewTree = chooseFormatters(getTree, formatName);
-  return getNewTree;
-};
-
-export default getDiff;
