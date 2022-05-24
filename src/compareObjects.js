8e9418ef83fa13264e01771afee8baca71878874
Idzanaagi <anmorgunowa@gmail.com>
2022-05-24 18:46:51 +0300

    compare3

--- src/compareObjects.js
+++ src/compareObjects.js
@@ -2,6 +2,30 @@ import _ from 'lodash';
 import { parsingFile } from '../parsers.js';
 import chooseFormatters from './formatters/index.js';
 
+function genDiff(filename1, filename2) {
+  const parse1 = JSON.parse(JSON.stringify(filename1));
+  const parse2 = JSON.parse(JSON.stringify(filename2));
+
+  const buidTree = (obj1, obj2) => {
+    const keys = Object.keys({ ...obj1, ...obj2 });
+    const sortedKeys = _.sortedUniq(_.sortBy(keys)).map((key) => {
+      if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+        return { type: 'nested', key, childs: buidTree(obj1[key], obj2[key]) };
+      } if (!Object.hasOwn(obj1, key)) {
+        return { type: 'added', key, value: obj2[key] };
+      } if (!Object.hasOwn(obj2, key)) {
+        return { type: 'removed', key, value: obj1[key] };
+      } if (obj1[key] === obj2[key]) {
+        return { type: 'same', key, value: obj1[key] };
+      }
+      return {
+        type: 'updated', key, valBefore: obj1[key], valAfter: obj2[key],
+      };
+    });
+    return sortedKeys;
+  };
+  return buidTree(parse1, parse2);
+}
 
 const getDiff = (filename1, filename2, formatName = 'stylish') => {
   const parseFile1 = parsingFile(filename1);
@@ -12,38 +36,3 @@ const getDiff = (filename1, filename2, formatName = 'stylish') => {
 };
 
 export default getDiff;
-
-function genDiff(filename1, filename2) {
-  const parse1 = JSON.parse(JSON.stringify(filename1));
-  const parse2 = JSON.parse(JSON.stringify(filename2))
-  const buidTree = (obj1, obj2) => {
-    const keys = Object.keys({ ...obj1, ...obj2 });
-    const sortedKeys = _.sortedUniq(_.sortBy(keys)).map((key) => {
-        if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-          return { type: 'nested', key, childs: buidTree(obj1[key], obj2[key]) };
-        } if (!Object.hasOwn(obj1, key)) {
-          return { type: 'added', key, value: obj2[key] };
-        } if (!Object.hasOwn(obj2, key)) {
-          return { type: 'removed', key, value: obj1[key] };
-        } if (obj1[key] === obj2[key]) {
-          return { type: 'same', key, value: obj1[key] };
-        }
-        return {
-          type: 'updated', key, valBefore: obj1[key], valAfter: obj2[key],
-        };
-      });
-    return sortedKeys;
-    
-  };
-  return buidTree(filename1, filename2);
-};
-
-/*
-const getDiff = (filename1, filename2, formatName = 'stylish') => {
-  const parseFile1 = parsingFile(filename1);
-  const parseFile2 = parsingFile(filename2);
-  const getTree = genDiff(parseFile1, parseFile2);
-  const getNewTree = chooseFormatters(getTree, formatName);
-  return getNewTree;
-};
-*/
